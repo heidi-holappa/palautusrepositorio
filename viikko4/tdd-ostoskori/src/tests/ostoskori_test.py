365b63642408dbc66b80518ceb13539ad0c6dc65
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-11-21 17:43:03 +0200

    After adding two items shopping cart has two items

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -68,3 +68,10 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(leipa)
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 2)
+
+    def test_kahden_saman_tuotteen_lisaamisen_jalkeen_ostoskorissa_on_yksi_tuote(self):
+        maito = Tuote("Maito", 3)
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 1)
