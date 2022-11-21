e1506503f8641094eb796a6179dc8ba32c7e64b8
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-11-21 17:54:07 +0200

    Removing the only item makes cart balance zero

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -108,3 +108,10 @@ class TestOstoskori(unittest.TestCase):
 
         ostokset = self.kori.ostokset()
         self.assertEqual(len(ostokset), 0)
+
+    def test_jos_koriin_lisatty_tuote_poistetaan_korin_hinta_on_nolla(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 0)
