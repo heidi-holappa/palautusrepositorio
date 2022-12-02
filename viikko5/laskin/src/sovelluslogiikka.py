d283779b81c14910b4838cb69992d17a866cb7e8
Heidi Holappa <heidi.holappa@helsinki.fi>
2022-12-02 15:41:35 +0200

    finish tasks 4 and 5

--- viikko5/laskin/src/sovelluslogiikka.py
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -1,15 +1,65 @@
 class Sovelluslogiikka:
     def __init__(self, tulos=0):
         self.tulos = tulos
+        self.kumoa = self.kumoa
 
-    def miinus(self, arvo):
-        self.tulos = self.tulos - arvo
+    def kumoa(self):
+        pass
 
-    def plus(self, arvo):
-        self.tulos = self.tulos + arvo
 
-    def nollaa(self):
-        self.tulos = 0
+class Summa:
 
-    def aseta_arvo(self, arvo):
-        self.tulos = arvo
+    def __init__(self, sovelluslogiikka: Sovelluslogiikka, io):
+        self.sovelluslogiikka = sovelluslogiikka
+        self.io = io
+        self.edellinen = 0
+
+    def suorita(self):
+        self.edellinen = self.sovelluslogiikka.tulos
+        self.sovelluslogiikka.tulos += int(self.io())
+        self.sovelluslogiikka.kumoa = self.kumoa
+
+    def kumoa(self):
+        self.sovelluslogiikka.tulos = self.edellinen
+
+
+class Erotus:
+
+    def __init__(self, sovelluslogiikka, io):
+        self.edellinen = 0
+        self.sovelluslogiikka = sovelluslogiikka
+        self.io = io
+
+    def suorita(self):
+        self.edellinen = self.sovelluslogiikka.tulos
+        self.sovelluslogiikka.tulos -= int(self.io())
+        self.sovelluslogiikka.kumoa = self.kumoa
+
+    def kumoa(self):
+        self.sovelluslogiikka.tulos = self.edellinen
+
+
+class Nollaus:
+
+    def __init__(self, sovelluslogiikka, io):
+        self.edellinen = 0
+        self.sovelluslogiikka = sovelluslogiikka
+        self.io = io
+
+    def suorita(self):
+        self.edellinen = self.sovelluslogiikka.tulos
+        self.sovelluslogiikka.tulos = 0
+        self.sovelluslogiikka.kumoa = self.kumoa
+
+    def kumoa(self):
+        self.sovelluslogiikka.tulos = self.edellinen
+
+
+class Kumoa:
+
+    def __init__(self, sovelluslogiikka, io):
+        self.sovelluslogiikka = sovelluslogiikka
+        self.io = io
+
+    def suorita(self):
+        self.sovelluslogiikka.kumoa()
